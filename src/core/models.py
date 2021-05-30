3e4d42bd9cfafd71b21d7c8cbad0fc4b9f88d577
Nick Hopewell <nicholashopewell@gmail.com>
2021-05-29 20:30:43 -0400

    define data model for warrior news post

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class WarriorPost(models.Model):
+    """
+    The data model for a Warrior class news post.
+
+    Fields:
+    -------
+        - original_source: CharField
+            The source of the post. Sources cane be on of:
+            '/r/wow/', '/r/classicwow/', 'Icy Veins', 'MMO Champion'.
+        - source_link: TextField
+            The link to the original source of the post.
+        - created: DateTimeField
+            The timestamp of when the post was scraped.
+        updated: DateTimeField
+            The timestamp of when the post was last updated.
+    """
+
+    class SourceType(models.TextChoices):
+       REDDIT_WOW = 'rwow', _('/r/wow/')
+       REDDIT_CLASSIC_WOW = 'rclass', _('/r/classicwow/')
+       ICY_VEINS = 'icy', _('Icy Veins')
+       MMO_CHAMPION = 'mmo', _('MMO Champion')
+ 
+    original_source = models.CharField(
+        max_length=20,
+        choices=SourceType.choices,
+    )
+    source_link = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.source_link[0:60]
+
+
