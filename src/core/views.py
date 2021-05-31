0f27c79f29cda692d29e6846a8263159a9ec4e83
Nick Hopewell <nicholashopewell@gmail.com>
2021-05-30 20:38:30 -0400

    add model ordering and pagination

--- src/core/views.py
+++ src/core/views.py
@@ -6,3 +6,5 @@ from .models import WarriorPost
 class WarriorPostListView(generic.ListView):
     model = WarriorPost
     template_name = 'warriorpost_list'
+    paginate_by = 20
+    ordering = ["-date_posted"]
